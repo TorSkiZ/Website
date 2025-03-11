74c64ce68490cd79c09938a36ffdaa6d37400cfb
Wiktor Wasiński <wiktor.wasinski@wiciu.pl>
2025-03-11 14:58:40 +0100

    seo (#2)

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-  import NavButton from './NavButton.vue'
-</script>
-
 <template>
   <div class="bg-third p-2 mx-8 rounded-2xl shadow-md">
     <nav class="flex gap-2">
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-  import HyperLink from '~/components/HyperLink.vue'
-
   useHead({
-    title: 'Home',
+    title: 'Homepage',
+    meta: [
+      {
+        name: 'description',
+        content: 'Homepage of my website.',
+      },
+    ],
   })
 </script>
 
 <template>
   <section class="bg-first p-8 rounded-2xl shadow-md">
-    <h1 class="text-3xl font-bold mb-4">Welcome to my website!</h1>
+    <h1 class="text-3xl font-bold mb-4">Homepage</h1>
     <p class="leading-relaxed">Under development...</p>
     <HyperLink href="https://github.com/TorSkiZ/Website" label="GitHub" />
   </section>
