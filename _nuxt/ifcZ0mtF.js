3fb8a7036877e6027b4c618b13e0e87887bb4b80
TorSkiZ <wiktor.wasinski@wiciu.pl>
2025-03-12 12:28:04 +0000

    Deploying to gh-pages from @ TorSkiZ/Website@360dffabd6671a5ac56ca00c007ab0f2201f5022 🚀

--- _nuxt/ifcZ0mtF.js
+++
@@ -1,1 +0,0 @@
-import{e as o,c as n,o as s,j as a}from"./H2RzIXKI.js";const c=["href"],l=o({__name:"HyperLink",props:{href:{type:String,required:!0}},setup(e){const r=e;return(t,i)=>(s(),n("a",{href:r.href,target:"_blank",class:"hover:text-second transition-colors duration-200 underline"},[a(t.$slots,"default")],8,c))}});export{l as _};
