b1329619f5f9979602d227f09fd6419f85d0d7f0
Wiktor Wasiński <wiktor.wasinski@wiciu.pl>
2025-03-14 00:49:45 +0100

    bugs (#8)

--- components/ColorModePicker.vue
+++ components/ColorModePicker.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="flex items-center">
-    <button
-      class="flex transition-colors duration-1000"
-      :class="$colorMode.value === 'dark' ? 'text-[#94908D]' : 'text-[#F09200]'"
-      @click="$colorMode.preference = $colorMode.value === 'dark' ? 'light' : 'dark'"
-    >
-      <IconifyIcon
-        :key="$colorMode.value"
-        :icon="
-          $colorMode.value === 'dark'
-            ? 'line-md:sunny-outline-to-moon-alt-loop-transition'
-            : 'line-md:moon-alt-to-sunny-outline-loop-transition'
-        "
-      />
-    </button>
-  </div>
+  <ClientOnly>
+    <div class="flex items-center">
+      <button
+        class="flex transition-colors duration-1000"
+        :class="$colorMode.value === 'dark' ? 'text-[#94908D]' : 'text-[#F09200]'"
+        @click="$colorMode.preference = $colorMode.value === 'dark' ? 'light' : 'dark'"
+      >
+        <IconifyIcon
+          :key="$colorMode.value"
+          :icon="
+            $colorMode.value === 'dark'
+              ? 'line-md:sunny-outline-to-moon-alt-loop-transition'
+              : 'line-md:moon-alt-to-sunny-outline-loop-transition'
+          "
+        />
+      </button>
+    </div>
+  </ClientOnly>
 </template>
