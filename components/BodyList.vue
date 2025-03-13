b02dc495f57b7f1e797c19da65c5dcd73f0d3995
Wiktor Wasiński <wiktor.wasinski@wiciu.pl>
2025-03-14 00:02:04 +0100

    set white color for list in light mode

--- components/BodyList.vue
+++ components/BodyList.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-  <ul class="list-disc pl-4 [&>li>p>span]:mr-2 [&>li>p>span]:align-middle">
+  <ul class="list-disc pl-4 [&>li>p>span]:mr-2 [&>li>p>span]:align-middle text-white">
     <li v-for="(child, index) in slots.default?.() ?? []" :key="index">
-      <component :is="child" />
+      <component :is="child" class="flex space-x-2" />
     </li>
   </ul>
 </template>
