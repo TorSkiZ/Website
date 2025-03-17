d53da5f4238039dce9115a8a435bb7926bae881c
Wiktor Wasiński <wiktor.wasinski@wiciu.pl>
2025-03-17 20:32:14 +0100

    refactor contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -9,32 +9,65 @@
     ],
   })
 
-  interface IconKeys {
-    email: number
-    linkedin: number
-    github: number
-    discord: number
-    buymeacoffee: number
-    youtube: number
-    instagram: number
-    facebook: number
-    spotify: number
-  }
+  const contacts = [
+    { id: 'email', href: 'mailto:wiktor.wasinski@wiciu.pl', icon: 'line-md:email', label: 'Email' },
+    {
+      id: 'linkedin',
+      href: 'https://www.linkedin.com/in/wiktor-wasi%C5%84ski-649118211/',
+      icon: 'line-md:linkedin',
+      label: 'LinkedIn',
+    },
+    { id: 'github', href: 'https://github.com/TorSkiZ', icon: 'line-md:github', label: 'GitHub' },
+    {
+      id: 'discord',
+      href: 'https://discordapp.com/users/927660826284937276/',
+      icon: 'line-md:discord',
+      label: 'Discord',
+    },
+    {
+      id: 'buymeacoffee',
+      href: 'https://buymeacoffee.com/torskiz',
+      icon: 'line-md:buy-me-a-coffee',
+      label: 'Buy Me a Coffee',
+    },
+    {
+      id: 'youtube',
+      href: 'https://www.youtube.com/@torskiz6115',
+      icon: 'line-md:youtube',
+      label: 'YouTube',
+    },
+    {
+      id: 'instagram',
+      href: 'https://www.instagram.com/wiktor.wasinski.torskiz/',
+      icon: 'line-md:instagram',
+      label: 'Instagram',
+    },
+    {
+      id: 'facebook',
+      href: 'https://www.facebook.com/wiktor.wasinski.torskiz/',
+      icon: 'line-md:facebook',
+      label: 'Facebook',
+    },
+    {
+      id: 'spotify',
+      href: 'https://open.spotify.com/user/wiktorwasinski',
+      icon: 'line-md:spotify',
+      label: 'Spotify',
+    },
+  ]
 
-  const iconKeys = ref<IconKeys>({
-    email: 0,
-    linkedin: 0,
-    github: 0,
-    discord: 0,
-    buymeacoffee: 0,
-    youtube: 0,
-    instagram: 0,
-    facebook: 0,
-    spotify: 0,
-  })
+  const iconKeys = ref(
+    contacts.reduce(
+      (acc, contact) => {
+        acc[contact.id] = 0
+        return acc
+      },
+      {} as Record<string, number>,
+    ),
+  )
 
-  function resetIcon(iconName: keyof IconKeys) {
-    iconKeys.value[iconName]++
+  const resetIcon = (id: string) => {
+    iconKeys.value[id] = (iconKeys.value[id] || 0) + 1
   }
 </script>
 
@@ -42,57 +75,16 @@
   <BodySection>
     <BodyHeader>Contact</BodyHeader>
     <BodyGrid>
-      <HyperLink href="mailto:wiktor.wasinski@wiciu.pl" @mouseenter="resetIcon('email')">
-        <IconifyIcon :key="iconKeys.email" icon="line-md:email" :size="56" />
-        Email
-      </HyperLink>
-      <HyperLink
-        href="https://www.linkedin.com/in/wiktor-wasi%C5%84ski-649118211/"
-        @mouseenter="resetIcon('linkedin')"
-      >
-        <IconifyIcon :key="iconKeys.linkedin" icon="line-md:linkedin" :size="56" />
-        LinkedIn
-      </HyperLink>
-      <HyperLink href="https://github.com/TorSkiZ" @mouseenter="resetIcon('github')">
-        <IconifyIcon :key="iconKeys.github" icon="line-md:github" :size="56" />
-        GitHub
-      </HyperLink>
-      <HyperLink
-        href="https://discordapp.com/users/927660826284937276/"
-        @mouseenter="resetIcon('discord')"
-      >
-        <IconifyIcon :key="iconKeys.discord" icon="line-md:discord" :size="56" />
-        Discord
-      </HyperLink>
-      <HyperLink href="https://buymeacoffee.com/torskiz" @mouseenter="resetIcon('buymeacoffee')">
-        <IconifyIcon :key="iconKeys.buymeacoffee" icon="line-md:buy-me-a-coffee" :size="56" />
-        Buy Me a Coffee
-      </HyperLink>
-      <HyperLink href="https://www.youtube.com/@torskiz6115" @mouseenter="resetIcon('youtube')">
-        <IconifyIcon :key="iconKeys.youtube" icon="line-md:youtube" :size="56" />
-        YouTube
-      </HyperLink>
-      <HyperLink
-        href="https://www.instagram.com/wiktor.wasinski.torskiz/"
-        @mouseenter="resetIcon('instagram')"
-      >
-        <IconifyIcon :key="iconKeys.instagram" icon="line-md:instagram" :size="56" />
-        Instagram
-      </HyperLink>
-      <HyperLink
-        href="https://www.facebook.com/wiktor.wasinski.torskiz/"
-        @mouseenter="resetIcon('facebook')"
-      >
-        <IconifyIcon :key="iconKeys.facebook" icon="line-md:facebook" :size="56" />
-        Facebook
-      </HyperLink>
-      <HyperLink
-        href="https://open.spotify.com/user/wiktorwasinski"
-        @mouseenter="resetIcon('spotify')"
-      >
-        <IconifyIcon :key="iconKeys.spotify" icon="line-md:spotify" :size="56" />
-        Spotify
-      </HyperLink>
+      <template v-for="contact in contacts" :key="contact.id">
+        <HyperLink
+          :href="contact.href"
+          class="flex flex-col items-center gap-1"
+          @mouseenter="resetIcon(contact.id)"
+        >
+          <IconifyIcon :key="iconKeys[contact.id]" :icon="contact.icon" :size="56" />
+          {{ contact.label }}
+        </HyperLink>
+      </template>
     </BodyGrid>
   </BodySection>
 </template>
