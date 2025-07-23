50c749327c35a5a92d78744494e7f6a747180bf3
TorSkiZ <wiktor.wasinski@wiciu.pl>
2025-07-23 15:32:06 +0200

    fix responsiveness for lists

--- components/BodyList.vue
+++ components/BodyList.vue
@@ -5,7 +5,10 @@
 <template>
   <ul class="list-disc pl-4 [&>li>p>span]:mr-2 [&>li>p>span]:align-middle text-white">
     <li v-for="(child, index) in slots.default?.() ?? []" :key="index">
-      <component :is="child" class="flex space-x-2" />
+      <component
+        :is="child"
+        class="flex flex-col space-y-1 sm:flex-row sm:space-y-0 sm:space-x-2"
+      />
     </li>
   </ul>
 </template>
